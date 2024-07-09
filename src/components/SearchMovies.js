fceefc35b0528d07148bccf9440d691d1ce86605
AndrewFlowe-rs <arnaldomahone@gmail.com>
2024-07-08 23:05:49 -0300

    temino desafio 1

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,110 +1,94 @@
+import React, { useEffect, useRef, useState } from "react";
+import noPoster from "../assets/images/no-image.png";
 
-import React, { useEffect, useRef, useState } from 'react';
-import noPoster from '../assets/images/no-image.png';
+function searchMovie(params) {
+  let apiKey = "2fe38164";
+  let [keyword, setKeyword] = useState([""]);
+  let [movies, setMovies] = useState([]);
+  let searchkey = useRef(null);
 
-function searchMovie (params) {
-	let apiKey = '2fe38164';
-	let [ keyword,setKeyword ] = useState([''])
-	let [ movies,setMovies] = useState ([]);
-	let searchkey = useRef(null);
-	
+  useEffect(() => {
+    const endPoint = `http://www.omdbapi.com/?apikey=${apiKey}&s=${keyword}`;
 
+    const getMovie = async () => {
+      const { Response, Search: movies } = await fetch(endPoint).then((res) =>
+        res.json()
+      );
+      Response && setMovies(movies);
+    };
+    getMovie();
+  }, [keyword]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setKeyword(searchkey.current.value);
+  };
 
-	useEffect (() => {
-		const endPoint =  `http://www.omdbapi.com/?apikey=${apiKey}&s=${keyword}`;
-		
-		const getMovie = async () => {
-			const { Response , Search :movies } = await fetch(endPoint).then(res => res.json());
-			Response && setMovies(movies)
-		};
-		getMovie();
-
-	
-	},[keyword]);
-
-	
-}
-
-
-
-import React from 'react';
-
-import noPoster from '../assets/images/no-poster.jpg';
-
-function SearchMovies(){
-
-	const movies = [
-		{
-			"Title": "Parchís",
-			"Year": "1983",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BYTgxNjg2MTAtYjhmYS00NjQwLTk1YTMtNmZmOTMyNTAwZWUwXkEyXkFqcGdeQXVyMTY5MDE5NA@@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Brigada en acción",
-			"Year": "1977",
-			"Poster": "N/A"
-		},
-	];
-
-	const keyword = 'PELÍCULA DEMO';
-
-	// Credenciales de API
-	const apiKey = 'X'; // Intenta poner cualquier cosa antes para probar
-
-	return(
-		<div className="container-fluid">
-			{
-				apiKey !== '' ?
-				<>
-					<div className="row my-4">
-						<div className="col-12 col-md-6">
-							{/* Buscador */}
-							<form method="GET">
-								<div className="form-group">
-									<label htmlFor="">Buscar por título:</label>
-									<input type="text" className="form-control" />
-								</div>
-								<button className="btn btn-info">Search</button>
-							</form>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12">
-							<h2>Películas para la palabra: {keyword}</h2>
-						</div>
-						{/* Listado de películas */}
-						{
-							movies.length > 0 && movies.map((movie, i) => {
-								return (
-									<div className="col-sm-6 col-md-3 my-4" key={i}>
-										<div className="card shadow mb-4">
-											<div className="card-header py-3">
-												<h5 className="m-0 font-weight-bold text-gray-800">{movie.Title}</h5>
-											</div>
-											<div className="card-body">
-												<div className="text-center">
-													<img 
-														className="img-fluid px-3 px-sm-4 mt-3 mb-4" 
-														src={movie.Poster}
-														alt={movie.Title} 
-														style={{ width: '90%', height: '400px', objectFit: 'cover' }} 
-													/>
-												</div>
-												<p>{movie.Year}</p>
-											</div>
-										</div>
-									</div>
-								)
-							})
-						}
-					</div>
-					{ movies.length === 0 && <div className="alert alert-warning text-center">No se encontraron películas</div>}
-				</>
-				:
-				<div className="alert alert-danger text-center my-4 fs-2">Eyyyy... ¿PUSISTE TU APIKEY?</div>
-			}
-		</div>
-	)
+  return (
+    <div className="container-fluid">
+      {apiKey !== "" ? (
+        <>
+          <div className="row my-4">
+            <div className="col-12 col-md-6">
+              {/* Buscador */}
+              <form method="GET" onSubmit={onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="">Buscar por título:</label>
+                  <input ref={searchkey} type="text" className="form-control" />
+                </div>
+                <button className="btn btn-info">Search</button>
+              </form>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <h2>Películas para la palabra: {keyword}</h2>
+            </div>
+            {/* Listado de películas */}
+            {movies &&
+              movies.map((movie, i) => {
+                return (
+                  <div className="col-sm-6 col-md-3 my-4" key={i}>
+                    <div className="card shadow mb-4">
+                      <div className="card-header py-3">
+                        <h5 className="m-0 font-weight-bold text-gray-800">
+                          {movie.Title}
+                        </h5>
+                      </div>
+                      <div className="card-body">
+                        <div className="text-center">
+                          <img
+                            className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                            src={
+                              movie.Poster !== "N/A" ? movie.Poster : noPoster
+                            }
+                            alt={movie.Title}
+                            style={{
+                              width: "90%",
+                              height: "400px",
+                              objectFit: "cover",
+                            }}
+                          />
+                        </div>
+                        <p>{movie.Year}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+          {movies === 0 && (
+            <div className="alert alert-warning text-center">
+              No se encontraron películas
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="alert alert-danger text-center my-4 fs-2">
+          Eyyyy... ¿PUSISTE TU APIKEY?
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default SearchMovies;
